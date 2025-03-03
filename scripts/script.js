9180ff15e157abceaf4bba9dd435bfc37f9aedec
Shahid Hasan Shuvo <mrshanshuvo@gmail.com>
2025-03-04 04:02:51 +0600

    completed all the challenge part of the project

--- scripts/script.js
+++ scripts/script.js
@@ -30,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
         taskAssignedElement.textContent = taskAssigned - 1;
         taskDoneElement.textContent = taskDone + 1;
         alert("Board updated successfully");
+        if (taskAssigned == 1) {
+          alert("Congrats !! You have completed all the current tasks.");
+        }
 
         // Fetch the task title from the current card (button's parent)
         const taskTitleElement = completedButton
@@ -43,9 +46,40 @@ document.addEventListener("DOMContentLoaded", function () {
         // Disable the button after clicking
         completedButton.disabled = true;
         completedButton.classList.add("opacity-50", "cursor-not-allowed");
+        completedButton.classList.remove("cursor-pointer");
       } else {
         alert("No tasks left to complete.");
       }
     });
   });
 });
+
+// current date
+const currentDate = new Date();
+
+// day of the week
+const dayOfWeek = currentDate.toLocaleString("en-US", { weekday: "short" });
+
+const formattedDate = currentDate.toLocaleDateString("en-US", {
+  month: "short",
+  day: "numeric",
+  year: "numeric",
+});
+
+document.getElementById("day-of-week").textContent = dayOfWeek + " ,";
+document.getElementById("current-date").textContent = formattedDate;
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeBackgroundColorRandomly() {
+  const body = document.body;
+
+  body.style.backgroundColor = getRandomColor();
+}
